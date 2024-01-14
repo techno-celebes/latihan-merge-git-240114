d30da01f23c86cce3405603b1a0b9b83d722a335
mlyhia <ichanuraziza17@gmail.com>
2024-01-14 15:04:03 +0800

    pricing2

--- resources/views/home/pricing.blade.php
+++ resources/views/home/pricing.blade.php
@@ -1,105 +1,51 @@
 @extends ('master.master_home')
 @section ('content')
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-* {
-  box-sizing: border-box;
-}
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 100px
+        }
 
-.columns {
-  float: left;
-  width: 33.3%;
-  padding: 8px;
-}
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
 
-.price {
-  list-style-type: none;
-  border: 1px solid #eee;
-  margin: 0;
-  padding: 0;
-  -webkit-transition: 0.3s;
-  transition: 0.3s;
-}
-
-.price:hover {
-  box-shadow: 0 8px 12px 0 rgba(0,0,0,0.2)
-}
-
-.price .header {
-  background-color: #111;
-  color: white;
-  font-size: 25px;
-}
-
-.price li {
-  border-bottom: 1px solid #eee;
-  padding: 20px;
-  text-align: center;
-}
-
-.price .grey {
-  background-color: #eee;
-  font-size: 20px;
-}
-
-.button {
-  background-color: #04AA6D;
-  border: none;
-  color: white;
-  padding: 10px 25px;
-  text-align: center;
-  text-decoration: none;
-  font-size: 18px;
-}
-
-@media only screen and (max-width: 600px) {
-  .columns {
-    width: 100%;
-  }
-}
-</style>
-</head>
-<body>
-
-<div class="columns">
-  <ul class="price">
-    <li class="header">Arabian Parfum</li>
-    <li class="grey">$ 9.99</li>
-    <li>10GB Storage</li>
-    <li>10 Emails</li>
-    <li>10 Domains</li>
-    <li>1GB Bandwidth</li>
-    <li class="grey"><a href="#" class="button">BUY</a></li>
-  </ul>
+        th {
+            background-color: black;
+            color: white;
+        }
+    </style>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <table>
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Parfum Premium</td>
+                    <td>Rp 50.000</td>
+                </tr>
+                <tr>
+                    <td>Parfum Arabia</td>
+                    <td>Rp 75.000</td>
+                </tr>
+                <tr>
+                    <td>Parfum Saja</td>
+                    <td>Rp 60.000</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="col-2"></div>
+    </div>
 </div>
-
-<div class="columns">
-  <ul class="price">
-    <li class="header" style="background-color:#04AA6D">Haram Parfum</li>
-    <li class="grey">$ 24.99</li>
-    <li>25GB Storage</li>
-    <li>25 Emails</li>
-    <li>25 Domains</li>
-    <li>2GB Bandwidth</li>
-    <li class="grey"><a href="#" class="button">BUY</a></li>
-  </ul>
-</div>
-
-<div class="columns">
-  <ul class="price">
-    <li class="header">Premium Parfum</li>
-    <li class="grey">$ 49.99</li>
-    <li>50GB Storage</li>
-    <li>50 Emails</li>
-    <li>50 Domains</li>
-    <li>5GB Bandwidth</li>
-    <li class="grey"><a href="#" class="button">BUY</a></li>
-  </ul>
-</div>
-
-</body>
-</html>
 @endsection
