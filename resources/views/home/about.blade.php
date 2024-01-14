997c02ef8ee53bd66a1fee8c33cb6440ac443e53
Ryu9145 <mohgiansaputra@gmail.com>
2024-01-14 14:20:11 +0700

    tentang

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -8,7 +8,7 @@
       <div class="row text-center text-md-left">
         <div class="col-md-5 col-lg-5 col-xl-5 mx-auto mt-3">
           <h5 class="font-weight-bold text-dark" style="text-align: left;">About</h5>
-          <p class="text-dark" style="text-align: left;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officiis perspiciatis vero laudantium illum sapiente! Sunt totam Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti facilis provident iure earum maxime in nesciunt reiciendis maiores facere mollitia eos nobis, ipsam dolores! Nisi, aliquid! Optio, sapiente molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aperiam distinctio tempora sint mollitia aut, molestiae corporis voluptatum repellat asperiores voluptatem sapiente consequuntur ad. Suscipit corporis a minus animi iste!</p>
+          <strong><p class="text-dark" style="text-align: left;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis officiis perspiciatis vero laudantium illum sapiente! Sunt totam Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti facilis provident iure earum maxime in nesciunt reiciendis maiores facere mollitia eos nobis, ipsam dolores! Nisi, aliquid! Optio, sapiente molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aperiam distinctio tempora sint mollitia aut, molestiae corporis voluptatum repellat asperiores voluptatem sapiente consequuntur ad. Suscipit corporis a minus</p></strong>
         </div>
         <div class="col-md-3 col-lg-3 col-xl-3 mx-auto">
           <img src="../../image/about.jpg" style="width: 100%;" alt="logo">
