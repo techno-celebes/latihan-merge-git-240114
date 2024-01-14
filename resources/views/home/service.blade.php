1d0c26104ffe5302dfdd7b129b8230eadb572ddb
unknown <tasrifaadman@gmail.com>
2024-01-14 15:20:39 +0000

    perubahan 2

--- resources/views/home/service.blade.php
+++ resources/views/home/service.blade.php
@@ -1,4 +1,15 @@
 @extends('master.master_home')
 @section('content')
-jjj
+<div class="container" style="margin-top: 100px;">
+  <div class="row">
+    <div class="col-4" style="background: skyblue;">
+      <div class="icon flex-shrink-0"><i class="fa-solid fa-cart-flatbed"></i></div>
+        <div>
+              <h4 class="title">Lorem Ipsum</h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
+       </div>
+    </div>
+  </div>
+</div>
 @endsection
