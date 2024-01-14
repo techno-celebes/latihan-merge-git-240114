b7e9098a937f731a66607f6fffb38190efeb3af5
Ryu9145 <mohgiansaputra@gmail.com>
2024-01-14 13:19:31 +0700

    tidak ada css

--- public/js/app.js
+++ public/js/app.js
@@ -49555,8 +49555,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\240114_merge_git\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\240114_merge_git\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\xampp\htdocs\240114_merge_git\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\240114_merge_git\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
